5a5176630df9b996bfc9dd6c532b8a8c5568749d
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-02-25 11:02:38 +0200

    create iterate()
    Created iterate(). For now it just prints all the entries in the folder.

--- sort.py
+++ sort.py
@@ -1,12 +1,20 @@
 import sys
+import os
+
+def iterate(path):
+    for entry in os.listdir(path):
+        print(entry)
+
 
 def main():
     try:
         path = sys.argv[1]
+        iterate(path)
     except IndexError:
         print("Error. Path to the folder is required")
         return
-    
-    print(path)
+    except FileNotFoundError:
+        print("Error. Specified path is not valid")
+        return
 
 main()
