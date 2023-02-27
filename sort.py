28385aa77bf3a15e85258a742194c2c18667e34c
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-02-27 14:56:03 +0200

    Create prepare()

--- sort.py
+++ sort.py
@@ -1,7 +1,21 @@
 import sys
 import os
 
+file_types = ("images", "video", "documents", "audio", "archives")
+
+def prepare(directory):
+    #Checks if folders for all the types of file exists
+    #and creates them if they do not exist
+    for type in file_types:
+        path = os.path.join(directory, type)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
 def iterate(root, relative_path = ""):
+    if not relative_path:
+        prepare(root)
+    #will execute only once in the beginning of iteration
+    
     dir_path = os.path.join(root, relative_path)
     for entry in os.listdir(dir_path):
         relative_entry_path = os.path.join(relative_path, entry)
