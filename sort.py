2dbfbc4a3de5689259857b372a6e6d14520620a6
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-02-28 14:26:01 +0200

    Update sort.py
    Now the script moves found audio files to folder "audio"

--- sort.py
+++ sort.py
@@ -8,6 +8,38 @@ file_types = {
     "audio": [".mp3", ".ogg", ".wav", ".amr"],
     "archives": [".zip", ".gz", ".tar"]}
 
+def move_audio(root, files):
+    destination = os.path.join(root, "audio")
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    for file in files:
+        file_path = os.path.join(root, file)
+        new_file_name = os.path.basename(file) #later names will be normalized here
+        new_file_path = os.path.join(destination, new_file_name)
+        os.rename(file_path, new_file_path)
+def move_video(root, files):
+    pass
+def move_images(root, files):
+    pass
+def move_documents(root, files):
+    pass
+def move_archives(root, files):
+    pass
+
+moving_functions = {
+    "images": move_images,
+    "video": move_video,
+    "documents": move_documents,
+    "audio": move_audio,
+    "archives": move_archives
+}
+
+def move_files(root, catalogue):
+    for type in file_types:
+        if catalogue[type]:
+            moving_functions[type](root, catalogue[type])
+
+
 def prepare(directory):
     #Checks if folders for all the types of file exists
     #and creates them if they do not exist
@@ -19,53 +51,52 @@ def prepare(directory):
 def log(sort_result):
     #sort_result - dict, that holds information about sorted files,
     #met extensions and unknown extensions
-    #inputs this information in files "catalogue.txt" in folders corresponding to each type
-    #and in files "found_extensions.txt" and "unknown_extensions.txt" in the root of the folder
+    #outputs this information into console
 
     #TODO
     print(sort_result)
 
-def iterate(root, relative_path = "", search_data = {}):
+def catalogue(root, relative_path = "", search_data = {}, ignore = []):
     #walks through the folder sorts files and records data about them
-    #returns dictionary, storing all the recorded data
+    #returns dictionary, with the recorded data
 
     if not search_data:
     #initializing the search data
         for type in file_types:
             search_data[type] = []
-        search_data["unknown"] = []
-        #a key for every file type, including those of unknown types
+        #a key for every file type
         search_data["found_extensions"] = set()
         search_data["unknown_extensions"] = set()
         #a key for found known extensions and unknown extensions
 
     dir_path = os.path.join(root, relative_path)
     for entry in os.listdir(dir_path):
-        if entry in file_types and not relative_path:
+        if entry in ignore:
             continue
         relative_entry_path = os.path.join(relative_path, entry)
         entry_path = os.path.join(dir_path, entry)
         if os.path.isdir(entry_path):
-            search_data = iterate(root, relative_path=relative_entry_path, search_data=search_data)
+            search_data = catalogue(root, relative_path=relative_entry_path, search_data=search_data)
         else:
             extension = os.path.splitext(entry_path)[1]
 
             is_type_recognized = False
             for type in file_types:
                 if extension in file_types[type]:
-                    search_data[type].append(entry_path)
+                    search_data[type].append(relative_entry_path)
                     search_data["found_extensions"].add(extension)
                     is_type_recognized = True
                     break
             if not is_type_recognized:
-                search_data["unknown"].append(entry_path)
                 search_data["unknown_extensions"].add(extension)
     
     return search_data
 
 def sort(path):
-    prepare(path)
-    log(iterate(path))
+    #prepare(path) obsolete
+    files = catalogue(path, ignore = file_types.keys())
+    log(files)
+    move_files(path, files)
 
 def main():
     try:
